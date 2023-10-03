f9d6b35ad4f1fe8f93fadb5137c8b2f2f2d64dd8
Zaid mangat <zaidmanghat248@gmail.com>
2023-10-03 23:02:09 +0500

    inboxScreenComplete

--- src/components/inboxScreen.js
+++ src/components/inboxScreen.js
@@ -14,7 +14,6 @@ import CallIcon from 'react-native-vector-icons/Ionicons';
 import ReportIcon from 'react-native-vector-icons/MaterialIcons';
 import DotIcon from 'react-native-vector-icons/Entypo';
 import SendIcon from 'react-native-vector-icons/MaterialCommunityIcons';
-import {red} from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
 
 const InboxHeader = ({navigation}) => {
   return (
@@ -57,14 +56,70 @@ const InboxHeader = ({navigation}) => {
 };
 const Inbox = ({navigation}) => {
   const msgsList = [
-    {
-      id: 1,
-      msg1: 'final price keya he. Last word what u demand',
-      msg2: '44000',
-    },
+    {id: '1', text: 'Hello!', isUser: true},
+    {id: '2', text: 'Hi there!', isUser: false},
+    {id: '3', text: 'How are you?', isUser: true},
+    {id: '4', text: 'I am good. Thanks!', isUser: false},
+    {id: '5', text: 'Hello!', isUser: true},
+    {id: '6', text: 'Hi there!', isUser: false},
+    {id: '7', text: 'How are you?', isUser: true},
+    {id: '8', text: 'I am good. Thanks!', isUser: false},
+    {id: '9', text: 'Hello!', isUser: true},
+    {id: '10', text: 'Hi there!', isUser: false},
+    {id: '11', text: 'How are you?', isUser: true},
+    {id: '12', text: 'I am good. Thanks!', isUser: false},
+
+    // {
+    //   id: 1,
+    //   msg1: 'final price keya he. Last word what u demand',
+    //   msg2: '44000',
+    //   sendBy: 'sender',
+    // },
+    // {
+    //   id: 1,
+    //   msg1: 'final price keya he. Last word what u demand',
+    //   msg2: '44000',
+    //   sendBy: 'reciever',
+    // },
+    // {
+    //   id: 1,
+    //   msg1: 'final price keya he. Last word what u demand',
+    //   msg2: '44000',
+    //   sendBy: 'sender',
+    // },
+    // {
+    //   id: 1,
+    //   msg1: 'final price keya he. Last word what u demand',
+    //   msg2: '44000',
+    //   sendBy: 'reciever',
+    // },
+    // {
+    //   id: 1,
+    //   msg1: 'final price keya he. Last word what u demand',
+    //   msg2: '44000',
+    //   sendBy: 'sender',
+    // },
+    // {
+    //   id: 1,
+    //   msg1: 'final price keya he. Last word what u demand',
+    //   msg2: '44000',
+    //   sendBy: 'reciever',
+    // },
+    // {
+    //   id: 1,
+    //   msg1: 'final price keya he. Last word what u demand',
+    //   msg2: '44000',
+    //   sendBy: 'sender',
+    // },
+    // {
+    //   id: 1,
+    //   msg1: 'final price keya he. Last word what u demand',
+    //   msg2: '44000',
+    //   sendBy: 'reciever',
+    // },
   ];
   return (
-    <SafeAreaView>
+    <SafeAreaView flex={1}>
       <InboxHeader />
       <View style={styles.detailview}>
         <Image
@@ -76,35 +131,42 @@ const Inbox = ({navigation}) => {
           <Text style={styles.pricetext}>PKR 45000 Thousand</Text>
         </View>
       </View>
-
-      <View style={{margin: 15}}>
+      <View style={{flex: 0.9, padding: 10}}>
         <FlatList
           data={msgsList}
           horizontal={false}
           renderItem={({item}) => (
-            <View>
-              <View style={styles.mesg1View}>
-                <Text style={styles.mesg1Text}>{item.msg1}</Text>
-              </View>
-              <View style={styles.mesg2View}>
-                <Text style={styles.mesg2Text}>{item.msg2}</Text>
-              </View>
+            // <View style={{flexDirection: 'row'}}>
+            //   {item.sendBy == 'sender' ? (
+            //     <View style={styles.mesg1View}>
+            //       <Text style={styles.mesg1Text}>{item.msg1}</Text>
+            //       <Text style={styles.mesg1Text}>{item.sendBy}</Text>
+            //     </View>
+            //   ) : (
+            //     <View style={styles.mesg2View}>
+            //       <Text style={styles.mesg2Text}>{item.msg2}</Text>
+            //       <Text style={styles.mesg1Text}>{item.sendBy}</Text>
+            //     </View>
+            //   )}
+            // </View>
+            <View
+              style={item.isUser ? styles.userMessage : styles.otherMessage}>
+              <Text style={styles.messageText}>{item.text}</Text>
             </View>
           )}
         />
       </View>
-      <View>
-        <View style={styles.inputcontainer}>
-          <TextInput placeholder="Send Message" style={styles.msginput} />
-          <TouchableOpacity>
-            <SendIcon
-              name="send-circle"
-              color="skyblue"
-              size={45}
-              style={styles.sendicon}
-            />
-          </TouchableOpacity>
-        </View>
+
+      <View style={styles.inputcontainer}>
+        <TextInput placeholder="Send Message" style={styles.msginput} />
+        <TouchableOpacity>
+          <SendIcon
+            name="send-circle"
+            color="#b63439"
+            size={45}
+            style={styles.sendicon}
+          />
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
@@ -150,42 +212,64 @@ const styles = StyleSheet.create({
     color: 'black',
     fontWeight: 'bold',
   },
-  mesg1View: {
-    height: 50,
-    width: 250,
-    backgroundColor: '#0096FF',
-    borderRadius: 8,
-    flex: 1,
-    marginLeft: 130,
-  },
-  mesg1Text: {
-    margin: 5,
-    fontSize: 15,
-  },
-  mesg2View: {
-    height: 40,
-    width: 60,
-    backgroundColor: '#D3D3D3',
-    borderRadius: 8,
-    marginTop: 10,
-  },
-  mesg2Text: {
-    margin: 8,
-    fontSize: 15,
-  },
+  // mesg1View: {
+  //   height: 50,
+  //   width: 250,
+  //   backgroundColor: '#0096FF',
+  //   borderRadius: 8,
+  //   position: 'absolute',
+  //   right: 0,
+  //   marginRight: 10,
+  // },
+  // mesg1Text: {
+  //   margin: 5,
+  //   fontSize: 15,
+  // },
+  // mesg2View: {
+  //   height: 40,
+  //   width: 70,
+  //   backgroundColor: '#D3D3D3',
+  //   borderRadius: 8,
+  //   marginTop: 50,
+  // },
+  // mesg2Text: {
+  //   margin: 8,
+  //   fontSize: 15,
+  // },
   inputcontainer: {
-    backgroundColor: 'white',
+    backgroundColor: '#fff',
     width: '100%',
     flexDirection: 'row',
     alignItems: 'center',
     position: 'absolute',
-    top: 485,
+    bottom: 0,
+    elevation: 5,
   },
   msginput: {
     flex: 1,
     backgroundColor: 'white',
     height: 40,
     margin: 10,
+  },
+  userMessage: {
+    alignSelf: 'flex-end',
+    backgroundColor: '#b63439',
+    padding: 10,
+    borderRadius: 10,
+    maxWidth: '70%',
+    marginBottom: 10,
+  },
+  otherMessage: {
+    alignSelf: 'flex-start',
+    backgroundColor: 'grey',
+    padding: 10,
+    borderRadius: 10,
+    maxWidth: '70%',
+    marginBottom: 10,
+  },
+  messageText: {
+    fontSize: 16,
+    color: '#fff',
   },
 });
 export default Inbox;
